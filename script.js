7344c887a2ba3fc28e8998df5200d62408ab8192
alianmc1988 <alianmc1988@gmail.com>
2021-05-22 01:09:58 -0400

    more commits

--- script.js
+++ script.js
@@ -5,21 +5,24 @@ let a = $form.addEventListener('submit',calculaElec);
 function calculaElec(event){
    
     event.preventDefault()
+    // Entrada del Form Data
     let form = new FormData(this);
     let anterior = form.get('kwanterior');
     let actual = form.get('kwactual');
+    
     let dif = actual-anterior;
     let arrdeslose = deslose(dif)
     let mony=0;
     let importe = [];
     let precios = [0.09,0.30,0.40,0.60,0.80,1.50,1.80,2.00,3.00,5.00]
+    // ==========Calculo de precios por intervalos==================
     for (let i = 0; i < precios.length; i++) {
         let multi = arrdeslose[i] * precios[i]
         importe.push(multi);
         mony = mony + multi ;
     }
   
-    
+    // ==============Llenado de tablas dinamicamente==============
     document.getElementById('resultconsumidos').append(`
     ${dif.toString()}kw
     `);
@@ -27,15 +30,16 @@ function calculaElec(event){
     ${mony.toString()}`)
     
     for (let index = 0; index < arrdeslose.length; index++) {
-        
+    
+    // // <li> Deslose de consumo por intervalos  
     let lideslose = document.createElement('li');
     lideslose.innerText = `${arrdeslose[index]}`
     document.getElementById('deslose').appendChild(lideslose )
-    
+    // <li> precios
     let liprecios = document.createElement('li');
     liprecios.innerText = `${precios[index]}`   
     document.getElementById('precio').appendChild(liprecios )  
-    
+    // <li> Importe
     let liImporte = document.createElement('li');
     liImporte.innerText = `${importe[index]}`   
     document.getElementById('importe').appendChild(liImporte )
@@ -51,7 +55,7 @@ let dif_Kw = (lec_ant,actual)=>{
     return actual-lec_ant;
     
 }
-// deslose de consumo por intervalos
+//funcion deslose de consumo por intervalos
 function deslose (consumo){
     let interval = [0,100,150,200,250,300,350,500,1000,5000]
     let valores = [100,50,50,50,50,50,150,500,4000,0]
