17e90a2bdcf484c492bc6f90cb3285a4e97d4410
alianmc1988 <alianmc1988@gmail.com>
2021-05-22 00:56:05 -0400

    ultimos retoques de diseño antes de deploy

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-// script cuenta electrica
+
+let $form = document.getElementById('consumoFormElec');
+let a = $form.addEventListener('submit',calculaElec);
+// Entrada de datos para calcular electricidad
+function calculaElec(event){
+   
+    event.preventDefault()
+    let form = new FormData(this);
+    let anterior = form.get('kwanterior');
+    let actual = form.get('kwactual');
+    let dif = actual-anterior;
+    let arrdeslose = deslose(dif)
+    let mony=0;
+    let importe = [];
+    let precios = [0.09,0.30,0.40,0.60,0.80,1.50,1.80,2.00,3.00,5.00]
+    for (let i = 0; i < precios.length; i++) {
+        let multi = arrdeslose[i] * precios[i]
+        importe.push(multi);
+        mony = mony + multi ;
+    }
+  
+    
+    document.getElementById('resultconsumidos').append(`
+    ${dif.toString()}kw
+    `);
+    document.getElementById('mony').append(` 
+    ${mony.toString()}`)
+    
+    for (let index = 0; index < arrdeslose.length; index++) {
+        
+    let lideslose = document.createElement('li');
+    lideslose.innerText = `${arrdeslose[index]}`
+    document.getElementById('deslose').appendChild(lideslose )
+    
+    let liprecios = document.createElement('li');
+    liprecios.innerText = `${precios[index]}`   
+    document.getElementById('precio').appendChild(liprecios )  
+    
+    let liImporte = document.createElement('li');
+    liImporte.innerText = `${importe[index]}`   
+    document.getElementById('importe').appendChild(liImporte )
+        
+        
+    }
+} 
 
 
-
+// Axuiliar functions
+// kilowats consumidos
 let dif_Kw = (lec_ant,actual)=>{
     return actual-lec_ant;
     
 }
-
+// deslose de consumo por intervalos
 function deslose (consumo){
     let interval = [0,100,150,200,250,300,350,500,1000,5000]
     let valores = [100,50,50,50,50,50,150,500,4000,0]
@@ -32,8 +77,14 @@ function deslose (consumo){
     }
    
 }
-function calcularya(){
-    let a = addEventListener('click',(e)=>{
-        e.preventDefault()
-    })
-}
+
+
+
+
+
+
+
+
+
+
+
