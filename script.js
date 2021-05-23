3d4c91d75ae3fc7fc0368379e4ced9ee98ebb0f4
alianmc1988 <alianmc1988@gmail.com>
2021-05-22 23:24:04 -0400

    corrigiendo bugs

--- script.js
+++ script.js
@@ -2,59 +2,55 @@
 let $form = document.getElementById('consumoFormElec');
 let a = $form.addEventListener('submit',calculaElec);
 // Entrada de datos para calcular electricidad
+
 function calculaElec(event){
-   
     event.preventDefault()
     // Entrada del Form Data
     let form = new FormData(this);
     let anterior = form.get('kwanterior');
     let actual = form.get('kwactual');
     
-    let dif = actual-anterior;
-    let arrdeslose = deslose(dif)
-    let mony=0;
-    let importe = [];
-    let precios = [0.09,0.30,0.40,0.60,0.80,1.50,1.80,2.00,3.00,5.00]
-    // ==========Calculo de precios por intervalos==================
-    for (let i = 0; i < precios.length; i++) {
-        let multi = arrdeslose[i] * precios[i]
-        importe.push(multi);
-        mony = mony + multi ;
-    }
-  
-    // ==============Llenado de tablas dinamicamente==============
-    document.getElementById('resultconsumidos').append(`
-    ${dif.toString()}kw
-    `);
-    document.getElementById('mony').append(` 
-    ${mony.toString()}`)
-    
-    for (let index = 0; index < arrdeslose.length; index++) {
-    
-    // // <li> Deslose de consumo por intervalos  
-    let lideslose = document.createElement('li');
-    lideslose.innerText = `${arrdeslose[index]}`
-    document.getElementById('deslose').appendChild(lideslose )
-    // <li> precios
-    let liprecios = document.createElement('li');
-    liprecios.innerText = `${precios[index]}`   
-    document.getElementById('precio').appendChild(liprecios )  
-    // <li> Importe
-    let liImporte = document.createElement('li');
-    liImporte.innerText = `${importe[index]}`   
-    document.getElementById('importe').appendChild(liImporte )
+    //Prevent negatives numbers || past lecture > than actual lecture
+    if (anterior<0 || actual<0) {
+        return(alert('numero negativo'))
+    }else if(anterior == 0 && actual == 0){
+        return(alert("Debe agregar valores antes de enviar"))
+    } 
+    else if(actual - anterior < 0){
+        return(alert('La lectura actual no puede ser menor a la anterior'))
         
+    }else {
+        let dif = actual-anterior;
+        let arrdeslose = deslose(dif)
+        let mony=0;
+        let importe = [];
+        let precios = [0.09,0.30,0.40,0.60,0.80,1.50,1.80,2.00,3.00,5.00]
+        // ==========Calculo de precios por intervalos==================
+        for (let i = 0; i < precios.length; i++) {
+            let multi = arrdeslose[i] * precios[i]
+            importe.push(multi);
+            mony = mony + multi ;
+        }
+      
+        //===========Llenado de los labels=======================
+        // document.getElementById('resultconsumidos').append(`
+        //     ${dif.toString()}kw `);
+        // document.getElementById('mony').append(` 
+        //     ${mony.toString()}`)
+        // ==============Llenado de tablas dinamicamente==============
+        let arr = arrTabla(arrdeslose,precios,importe);
+        renderTabla(arr);
         
+        let labelConsumo = `<label><b>Consumidos:</b>${dif.toString()}kw</label></br>`
+        let labelPago = `<label><b>A Pagar:$</b>${mony.toString()}</label>`
+        document.getElementById('consumoInfo').innerHTML = labelConsumo + labelPago
     }
-} 
-
-
-// Axuiliar functions
-// kilowats consumidos
-let dif_Kw = (lec_ant,actual)=>{
-    return actual-lec_ant;
     
 }
+
+
+
+// // Axuiliar functions
 //funcion deslose de consumo por intervalos
 function deslose (consumo){
     let interval = [0,100,150,200,250,300,350,500,1000,5000]
@@ -81,6 +77,35 @@ function deslose (consumo){
     }
    
 }
+
+// Crea arreglo anidado para generar Tabla
+function arrTabla (arr1,arr2,arr3){
+    let arrResult=[];
+    
+    for (let i = 0; i < arr1.length; i++) {
+        let aux = [arr1[i],arr2[i],arr3[i]]
+        arrResult.push(aux)
+    }
+    return arrResult;
+}
+
+// Genera una tabla dinamicamente de 3 columnas (deslose,precios,resultados) a partir de un arreglo anidado de elementos
+function renderTabla(arr){
+   let tb = "<thead><tr> <th>Deslose</th><th>Precio</th><th>Importe</th></tr></thead><tbody>"
+                
+    for(let element of arr){
+        tb += `<tr><td>${element[0]}</td><td>${element[1]}</td><td>${element[2]}</td></tr>`
+    }
+    
+    tb+="</tbody>" 
+    
+    document.getElementById("factura").innerHTML = tb;
+    
+}
+
+
+
+
 
 
 
